2826df582157d9cdfab724352957b4a61ee7ed8f
Hasan Kumar Reddy A <mintuhouse@gmail.com>
2012-04-12 11:36:24 +0530

    Added Firebird Code

---
+++ firebird/lcd.c
@@ -0,0 +1,289 @@
+
+/**
+Authors:
+
+Group 17:
+---------
+
+ANUP NAIK 10305915 
+VISHNU KANTH 10305020 
+RAJ DEEPAK 10305081
+
+AVR Studio Version 4.17, Build 666
+
+Date: 8th November 2010
+
+This project is about developing a robot which can collect balls randomly scattered around in an arena.
+
+Concepts covered: Image Processing, Wireless Communication through Zigbee and Interrupt Handling.
+
+Note:
+
+1. Make sure that in the configuration options following settings are done for proper operation of the code
+
+Microcontroller: atmega2560 Frequency: 11059200 Optimization: -O0 (For more information read section: Selecting proper optimization options below figure 4.22 in the hardware manual)
+
+2. The matlab code uses a serial port for communicating data through zigbee module. This port number must be changed accordingly. 
+
+*********************************************************************************/
+/********************************************************************************
+
+   Copyright (c) 2010, ERTS Lab IIT Bombay erts@cse.iitb.ac.in               -*- c -*-
+   All rights reserved.
+
+   Redistribution and use in source and binary forms, with or without
+   modification, are permitted provided that the following conditions are met:
+
+   * Redistributions of source code must retain the above copyright
+     notice, this list of conditions and the following disclaimer.
+
+   * Redistributions in binary form must reproduce the above copyright
+     notice, this list of conditions and the following disclaimer in
+     the documentation and/or other materials provided with the
+     distribution.
+
+   * Neither the name of the copyright holders nor the names of
+     contributors may be used to endorse or promote products derived
+     from this software without specific prior written permission.
+
+   * Source code can be used for academic purpose. 
+	 For commercial use permission form the author needs to be taken.
+
+  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+  AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+  IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+  ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+  LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+  CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+  SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+  INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+  CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+  ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+  POSSIBILITY OF SUCH DAMAGE. 
+
+  Software released under Creative Commence cc by-nc-sa licence.
+  For legal information refer to: 
+  http://creativecommons.org/licenses/by-nc-sa/3.0/legalcode
+
+********************************************************************************/
+
+#include <avr/io.h>
+#include <avr/delay.h>
+#include <util/delay.h>
+
+#define FCPU 11059200ul
+#define RS 0
+#define RW 1
+#define EN 2
+#define lcd_port PORTC
+
+#define sbit(reg,bit)	reg |= (1<<bit)
+#define cbit(reg,bit)	reg &= ~(1<<bit)
+
+void init_ports();
+void lcd_reset_4bit();
+void lcd_init();
+void lcd_wr_command(unsigned char);
+void lcd_wr_char(char);
+void lcd_home();
+void lcd_cursor(char, char);
+void lcd_print(char, char, unsigned int, int);
+void lcd_string(char*);
+
+unsigned int temp;
+unsigned int unit;
+unsigned int tens;
+unsigned int hundred;
+unsigned int thousand;
+unsigned int million;
+
+int i;
+
+
+/*****Function to Reset LCD*****/
+void lcd_set_4bit()
+{
+	_delay_ms(1);
+
+	cbit(lcd_port,RS);				//RS=0 --- Command Input
+	cbit(lcd_port,RW);				//RW=0 --- Writing to LCD
+	lcd_port = 0x30;				//Sending 3
+	sbit(lcd_port,EN);				//Set Enable Pin
+	_delay_ms(5);					//Delay
+	cbit(lcd_port,EN);				//Clear Enable Pin
+
+	_delay_ms(1);
+
+	cbit(lcd_port,RS);				//RS=0 --- Command Input
+	cbit(lcd_port,RW);				//RW=0 --- Writing to LCD
+	lcd_port = 0x30;				//Sending 3
+	sbit(lcd_port,EN);				//Set Enable Pin
+	_delay_ms(5);					//Delay
+	cbit(lcd_port,EN);				//Clear Enable Pin
+
+	_delay_ms(1);
+
+	cbit(lcd_port,RS);				//RS=0 --- Command Input
+	cbit(lcd_port,RW);				//RW=0 --- Writing to LCD
+	lcd_port = 0x30;				//Sending 3
+	sbit(lcd_port,EN);				//Set Enable Pin
+	_delay_ms(5);					//Delay
+	cbit(lcd_port,EN);				//Clear Enable Pin
+
+	_delay_ms(1);
+
+	cbit(lcd_port,RS);				//RS=0 --- Command Input
+	cbit(lcd_port,RW);				//RW=0 --- Writing to LCD
+	lcd_port = 0x20;				//Sending 2 to initialise LCD 4-bit mode
+	sbit(lcd_port,EN);				//Set Enable Pin
+	_delay_ms(5);					//Delay
+	cbit(lcd_port,EN);				//Clear Enable Pin
+
+	
+}
+
+/*****Function to Initialize LCD*****/
+void lcd_init()
+{
+	_delay_ms(1);
+
+	lcd_wr_command(0x28);			//LCD 4-bit mode and 2 lines.
+	lcd_wr_command(0x01);
+	lcd_wr_command(0x06);
+	lcd_wr_command(0x0E);
+	lcd_wr_command(0x80);
+		
+}
+
+	 
+/*****Function to Write Command on LCD*****/
+void lcd_wr_command(unsigned char cmd)
+{
+	unsigned char temp;
+	temp = cmd;
+	temp = temp & 0xF0;
+	lcd_port &= 0x0F;
+	lcd_port |= temp;
+	cbit(lcd_port,RS);
+	cbit(lcd_port,RW);
+	sbit(lcd_port,EN);
+	_delay_ms(5);
+	cbit(lcd_port,EN);
+	
+	cmd = cmd & 0x0F;
+	cmd = cmd<<4;
+	lcd_port &= 0x0F;
+	lcd_port |= cmd;
+	cbit(lcd_port,RS);
+	cbit(lcd_port,RW);
+	sbit(lcd_port,EN);
+	_delay_ms(5);
+	cbit(lcd_port,EN);
+}
+
+/*****Function to Write Data on LCD*****/
+void lcd_wr_char(char letter)
+{
+	char temp;
+	temp = letter;
+	temp = (temp & 0xF0);
+	lcd_port &= 0x0F;
+	lcd_port |= temp;
+	sbit(lcd_port,RS);
+	cbit(lcd_port,RW);
+	sbit(lcd_port,EN);
+	_delay_ms(5);
+	cbit(lcd_port,EN);
+
+	letter = letter & 0x0F;
+	letter = letter<<4;
+	lcd_port &= 0x0F;
+	lcd_port |= letter;
+	sbit(lcd_port,RS);
+	cbit(lcd_port,RW);
+	sbit(lcd_port,EN);
+	_delay_ms(5);
+	cbit(lcd_port,EN);
+}
+
+
+void lcd_home()
+{
+	lcd_wr_command(0x80);
+}
+
+
+/*****Function to Print String on LCD*****/
+void lcd_string(char *str)
+{
+	while(*str != '\0')
+	{
+		lcd_wr_char(*str);
+		str++;
+	}
+}
+
+/*** Position the LCD cursor at "row", "column". ***/
+
+void lcd_cursor (char row, char column)
+{
+	switch (row) {
+		case 1: lcd_wr_command (0x80 + column - 1); break;
+		case 2: lcd_wr_command (0xc0 + column - 1); break;
+		case 3: lcd_wr_command (0x94 + column - 1); break;
+		case 4: lcd_wr_command (0xd4 + column - 1); break;
+		default: break;
+	}
+}
+
+/***** Function To Print Any input value upto the desired digit on LCD *****/
+void lcd_print (char row, char coloumn, unsigned int value, int digits)
+{
+	unsigned char flag=0;
+	if(row==0||coloumn==0)
+	{
+		lcd_home();
+	}
+	else
+	{
+		lcd_cursor(row,coloumn);
+	}
+	if(digits==5 || flag==1)
+	{
+		million=value/10000+48;
+		lcd_wr_char(million);
+		flag=1;
+	}
+	if(digits==4 || flag==1)
+	{
+		temp = value/1000;
+		thousand = temp%10 + 48;
+		lcd_wr_char(thousand);
+		flag=1;
+	}
+	if(digits==3 || flag==1)
+	{
+		temp = value/100;
+		hundred = temp%10 + 48;
+		lcd_wr_char(hundred);
+		flag=1;
+	}
+	if(digits==2 || flag==1)
+	{
+		temp = value/10;
+		tens = temp%10 + 48;
+		lcd_wr_char(tens);
+		flag=1;
+	}
+	if(digits==1 || flag==1)
+	{
+		unit = value%10 + 48;
+		lcd_wr_char(unit);
+	}
+	if(digits>5)
+	{
+		lcd_wr_char('E');
+	}
+	
+}
+		
